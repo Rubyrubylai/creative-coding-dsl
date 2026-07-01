7270e550c0e2b9e16c4d791c127508fef2cb811b
Rubyrubylai <r844312@gmail.com>
2026-07-01 17:22:33 +0200

    feat: add randomShape

---
+++ src/crode/build/randomShape/sketch.js
@@ -0,0 +1,41 @@
+// Generated from canvas "RandomShape"
+
+function bubble() {
+  fill("blue");
+  noStroke();
+  circle(0, 0, ((random(20., 40.) / 2.) * 2.0));
+}
+
+function stars() {
+  fill("yellow");
+  noStroke();
+  {
+    let outerR = (random(0., 30.));
+    let innerR = outerR * 0.4;
+    beginShape();
+    for (let i = 0; i < 5 * 2; i++) {
+      let angle = (Math.PI / 5) * i - Math.PI / 2;
+      let rad = (i % 2 === 0) ? outerR : innerR;
+      vertex(rad * Math.cos(angle), rad * Math.sin(angle));
+    }
+    endShape(CLOSE);
+  }
+}
+
+function setup() {
+  createCanvas(800., 600.);
+  angleMode(DEGREES);
+  background(255);
+
+  if ((random(0., 1.) < 0.95)) {
+  push();
+  translate((random(0., 780.)), (random(0., 200.)));
+  stars();
+  pop();
+  } else {
+  push();
+  translate((random(0., 780.)), (random(0., 200.)));
+  bubble();
+  pop();
+  }
+}
